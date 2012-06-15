3a304d67c2aa1ab2e4c46937500d29b70d6db59e
Krzysztof Zylawy <krzysztof@picklive.com>
2012-06-15 02:43:22 +0100

    a step - is it the right direction?

--- scala/day3/excersises/sizer.scala
+++ scala/day3/excersises/sizer.scala
@@ -4,12 +4,25 @@ import Actor._
 import scala.util.matching.Regex
 
 object PageLoader {
-  def getPageSizeAndLinks(url: String) =  {
+  def getPageSizeAndLinks(url: String, findLinks: Boolean) =  {
     println("getting url: "+url)
-    val html = Source.fromURL(url).mkString
-    val linkRegex = new Regex("""<a.*href="http://((\w|\:|\/|\?|\=|\.|\&)+)"""","url")
-    val matchData = linkRegex.findAllIn(html).matchData
-    val links = matchData.map{a => "http://"+a.group("url")}.toList
+
+    val html = try {
+      Source.fromURL(url).mkString
+    } catch {
+      case _ => {
+        println("Url "+url+" could not be loaded")
+        ""
+      }
+    }
+    
+    val links = if(findLinks) {
+      val linkRegex = new Regex("""<a.*href="http://((\w|\:|\/|\?|\=|\.|\&)+)"""","url")
+      val matchData = linkRegex.findAllIn(html).matchData
+      matchData.map{a => "http://"+a.group("url")}.toList
+    } else {
+      List()
+    }
 
     println("Size for "+url+": "+html.length)
     println("Number of links for "+url+": "+links.length)
@@ -33,32 +46,37 @@ def timeMethod(method: () => Unit) = {
 
 def getPageSizeSequentially() = {
   for(url <- urls) {
-    val (size, links) = PageLoader.getPageSizeAndLinks(url)
+    val (size, links) = PageLoader.getPageSizeAndLinks(url, true)
     links.foreach(println)
-    links.foreach{ link =>
-      val (link_size, link_links) = PageLoader.getPageSizeAndLinks(link)
+    val total = links.foldLeft(size) { (sum,link) =>
+      val (link_size, link_links) = PageLoader.getPageSizeAndLinks(link, false)
+      sum + link_size
     }
-    //println("Total size: "+linksSize.foldLeft(size){ (sum,ls) => sum + ls})
+    println("Total size: "+total)
   }
 }
 
-def getPageSizeConcurrently() = {
+def getPageSizeConcurrently(urls:List[String], follow:Boolean):Any = {
   var caller = self
   for(url <- urls) {
-   actor { caller ! (url, PageLoader.getPageSizeAndLinks(url)) }
+   actor { caller ! (url, PageLoader.getPageSizeAndLinks(url, true)) }
   }
 
   for(url <- urls) {
     receive {
-      case (url, (size,links)) =>
-        println("Received")
+      case (url, (size,links:List[String])) =>
+        println("Finished: "+url)
+        if(follow) {
+          getPageSizeConcurrently(links, false)
+        }
     }
   }
 }
 
-println("Sequential run: ")
-timeMethod{getPageSizeSequentially}
+//println("Sequential run: ")
+//timeMethod{getPageSizeSequentially}
 
 println("Concurrent run: ")
-timeMethod{getPageSizeConcurrently}
+//timeMethod{getPageSizeConcurrently(urls, true)}
+getPageSizeConcurrently(urls, true)
 
